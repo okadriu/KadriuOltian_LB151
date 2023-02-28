ffa17629cedbd5aa1ac1066a5cc9f6b1c20413e2
Oltian Kadriu <69577043+okadriu@users.noreply.github.com>
2023-02-28 11:26:12 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD-Q9__ux2ZJRjXSr-lElSkRX1vWZUyJKQ",
@@ -11,5 +12,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
-export const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
